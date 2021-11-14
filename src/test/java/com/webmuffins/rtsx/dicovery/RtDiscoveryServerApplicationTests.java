5d6d72d40a1ef60ecc2cb914862f44ca823c5534
Andrii Makrushyn <a.makrushyn@uaiev8503nbl.levi9.com>
2021-11-14 17:34:50 +0200

    [Project] Initial commit

---
+++ src/test/java/com/webmuffins/rtsx/dicovery/RtDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmuffins.rtsx.dicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
